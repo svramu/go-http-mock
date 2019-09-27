b921a78f6000e02b14f6d0f77af77d5749d6c87c
sv ramu <svramu@gmail.com>
2019-09-27 14:55:00 +0530

    0.2 Yaml file with struct possible.

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v3"
+)
+
+type conf struct {
+	Hits int64 `yaml:"hits"`
+	Time int64 `yaml:"time"`
+}
+
+func (c *conf) getConf(pathToFile string) *conf {
+
+	yamlFile, err := ioutil.ReadFile(pathToFile)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return c
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	var c conf
+	c.getConf("conf.yaml")
+
+	fmt.Println(c)
 }

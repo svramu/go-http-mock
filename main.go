6dfaf30c3c10286a9ce674b2602dfd9bd10c165c
sv ramu <svramu@gmail.com>
2019-09-27 15:28:55 +0530

    0.3 Template golang working.

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
-type conf struct {
-	Hits int64 `yaml:"hits"`
-	Time int64 `yaml:"time"`
+// Rule ...
+type Rule struct {
+	Request  string
+	Callback string
+	Delay    int // In Millis
 }
 
-func (c *conf) getConf(pathToFile string) *conf {
+// Conf ...
+type Conf struct {
+	Env   map[string]string
+	Rules []Rule
+}
 
+func (c *Conf) parse(pathToFile string) *Conf {
 	yamlFile, err := ioutil.ReadFile(pathToFile)
 	if err != nil {
 		log.Printf("yamlFile.Get err   #%v ", err)
@@ -23,13 +32,19 @@ func (c *conf) getConf(pathToFile string) *conf {
 	if err != nil {
 		log.Fatalf("Unmarshal: %v", err)
 	}
-
 	return c
 }
 
 func main() {
-	var c conf
-	c.getConf("conf.yaml")
+	var c Conf
+	c.parse("conf.yaml")
 
 	fmt.Println(c)
+
+	for i, r := range c.Rules {
+		fmt.Println(i, r.Callback)
+		t := template.Must(template.New("callback").Parse(r.Callback))
+		t.Execute(os.Stdout, c.Env)
+		fmt.Println()
+	}
 }
